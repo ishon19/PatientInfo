ec9cd2f65e38393c11fb04fec1a9e96ef8880d2d
ishon19 <pathak.shreyans@gmail.com>
2020-11-22 05:54:43 +0530

    mongodb linked

--- patientInfoBackend/index.js
+++ patientInfoBackend/index.js
@@ -5,6 +5,7 @@ const multer = require("multer");
 const csv = require("csvtojson");
 const morgan = require("morgan");
 const xlsx = require("xlsx");
+const Patient = require("./models/Patient");
 const app = express();
 const fileUpload = multer();
 
@@ -22,7 +23,7 @@ app.use(
   )
 );
 
-let data = [];
+//let data = [];
 
 //Routes
 app.get("/", (request, response) => {
@@ -30,24 +31,33 @@ app.get("/", (request, response) => {
 });
 
 app.get("/patients", (request, response) => {
-  if (data.length > 0) response.status(200).json(data);
-  else response.status(400).json({ error: "No Data Found" });
+  /* if (data.length > 0) response.status(200).json(data);
+  else response.status(400).json({ error: "No Data Found" }); */
+  Patient.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
-app.get("/patients/:id", (request, response) => {
-  const id = request.params.id;
+app.get("/patients/:id", (request, response, next) => {
+  /* const id = request.params.id;
   console.log("ID", id, "Data:", data);
   if (id) {
     const patientInfo = data.filter((patient) => patient.id === id);
     response.status(200).json(patientInfo);
   } else {
     response.status(400).json({ error: "No Data Found" });
-  }
+  } */
+  Patient.findById(request.params.id)
+    .then((patient) => {
+      console.log("Patient: ", patient);
+      return response.json(patient);
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/patients", fileUpload.single("Data"), (request, response) => {
+app.post("/patients", fileUpload.single("Data"), (request, response, next) => {
   console.log("UploadInfo: ", request.file);
-  data = [];
+  //data = [];
   const mimeType = request.file.mimetype;
   if (mimeType.includes("csv")) {
     const csvString = request.file.buffer.toString("utf-8");
@@ -55,8 +65,36 @@ app.post("/patients", fileUpload.single("Data"), (request, response) => {
       .fromString(csvString)
       .then((rows) => {
         console.log(rows);
-        data = rows;
-        response.status(200).json(rows);
+        /* data = rows;
+        response.status(200).json(rows); */
+        if (rows) {
+          //clear the database before uploading data
+          Patient.deleteMany({});
+          rows.forEach((patientObj, index, arr) => {
+            let patient = new Patient({
+              name: patientObj?.name ?? "",
+              age: patientObj?.age ?? -1,
+              gender: patientObj?.gender ?? "",
+              contact: patientObj?.contact ?? 0,
+              address: patientObj?.address ?? "",
+            });
+
+            patient
+              .save()
+              .then((savedPatient) => {
+                console.log("Saved Patient: ", savedPatient);
+                if (index == arr.length - 1) {
+                  response.status(200).json(rows);
+                }
+              })
+              .catch((error) => {
+                console.log("Error while saving");
+                next(error);
+              });
+          });
+        } else {
+          response.status(400).json({ error: "Something went wrong" });
+        }
       })
       .catch((error) => console.error(error));
   } else {
@@ -67,13 +105,54 @@ app.post("/patients", fileUpload.single("Data"), (request, response) => {
       let sheet = xlsObject.Sheets[sheetName];
       let sheetData = xlsx.utils.sheet_to_json(sheet);
       console.log("Sheet Data", sheetData);
-      data = sheetData;
-      response.status(200).json(sheetData);
+      //data = sheetData;
+      //response.status(200).json(sheetData);
+      if (sheetData) {
+        //clear the database before uploading data
+        Patient.deleteMany({});
+        sheetData.forEach((patientObj, index, arr) => {
+          let patient = new Patient({
+            name: patientObj?.name ?? "",
+            age: patientObj?.age ?? -1,
+            gender: patientObj?.gender ?? "",
+            contact: patientObj?.contact ?? 0,
+            address: patientObj?.address ?? "",
+          });
+
+          patient
+            .save()
+            .then((savedPatient) => {
+              console.log("Saved Patient: ", savedPatient);
+              if (index == arr.length - 1) {
+                response.status(200).json(sheetData);
+              }
+            })
+            .catch((error) => {
+              console.log("Error while saving");
+              next(error);
+            });
+        });
+      } else {
+        response.status(400).json({ error: "Something went wrong" });
+      }
     } catch (error) {
       response.status(400).json({ error: "Something went wrong" });
     }
   }
 });
+
+//Error Handling middleware
+const errorHandler = (error, request, response, next) => {
+  console.log("[ErrorHandler] An Error Occured: ", error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformed id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
---
+++ patientInfoBackend/models/Patient.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const url = process.env.MONGODB_URI;
+console.log("The connection URL is", url);
+
+mongoose
+  .connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((response) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => console.log("Error connecting to the database: ", error));
+
+const patientSchema = mongoose.Schema({
+  name: { type: String, required: true, minlength: 3 },
+  age: {type: Number, required: true},
+  gender: {type: String, required: true},
+  phone: { type: Number, required: true, minlength: 10, unique: true },
+  address: {type: String, required: true}
+});
+
+patientSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+patientSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Patient", patientSchema);
